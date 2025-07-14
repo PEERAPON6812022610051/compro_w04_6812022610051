41e389a9eb79a427edf6a97dfdc324a83878e9d3
PEERAPON6812022610051 <s6812022610051@email.kmutnb.ac.th>
2025-07-14 17:27:38 +0700

    Bright

---
+++ coding_w04-03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char  nameInitial = 'B';
+    int age = 18;
+    float weight = 51.4;
+    char gender[] = "maie";
+
+    printf("nameInitial: %c\n", nameInitial);
+    printf("age: %d\n", age);
+    printf("weight: %f\n", weight);
+    printf("gender: %s\n", gender);
+    return 0; 
+
+}
