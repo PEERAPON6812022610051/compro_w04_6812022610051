dd7704100fd2e70e4127b4c360d4c4faa3785aee
PEERAPON6812022610051 <s6812022610051@email.kmutnb.ac.th>
2025-07-14 17:04:20 +0700

    Bright

---
+++ coding_w04-01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  
+
+int main() {    // int(keywords) main(Identifiers)
+    printf("Hallo,"); // printf(Identifier)  ;(Operator)
+    printf("C Tokens !");  // printf(Identifier) ;(Operator)
+    return 0;  // return(keywords) ;
+
+}
