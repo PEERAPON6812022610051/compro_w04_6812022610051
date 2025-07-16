22e1353761a376157261cc068596a7b6b9000832
PEERAPON6812022610051 <peeraponrodma@gmail.com>
2025-07-16 17:42:59 +0700

    Add files via upload

---
+++ coding_w04-01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  // include(keyword)
+
+int main() {    // int(keywords) main(Identifiers)
+    printf("Hello,"); // printf(Identifier)  ;(Operator)
+    printf("C Tokens !");  // printf(Identifier) ;(Operator)
+    return 0;  // return(keywords) 0(Constants) ;(Operator)
+
+}
