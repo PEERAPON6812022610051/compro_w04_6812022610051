778e499aba740488fe3642a970033fbb4b9fb4ea
PEERAPON6812022610051 <peeraponrodma@gmail.com>
2025-07-16 17:41:14 +0700

    Delete coding_w04-01.c

--- coding_w04-01.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>  
-
-int main() {    // int(keywords) main(Identifiers)
-    printf("Hallo,"); // printf(Identifier)  ;(Operator)
-    printf("C Tokens !");  // printf(Identifier) ;(Operator)
-    return 0;  // return(keywords) ;
-
-}
