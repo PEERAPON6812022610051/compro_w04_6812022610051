9ae6ba127233d9aae4c2b14e093e516ae9271c8c
PEERAPON6812022610051 <peeraponrodma@gmail.com>
2025-07-20 21:07:26 +0700

    Delete coding_w04-02.c

--- coding_w04-02.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> // (ไม่มี # หน้าinclude) (ไม่มี<>หน้าเเละหลังstdio.h)
-
-int main(){    
-    int a = 10;   // ไม่มี ; ลงท้าย
-    printf("a = %d", a);  // ไม่มี ; ลงท้าย
-    return 0; // ไม่มี ; ลงท้าย
-}
